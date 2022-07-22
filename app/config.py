e8beff1ae49e6bd25082c55b3b4a8749d635efa1
wanguiwaweru <wanguiwawerub@gmail.com>
2022-07-22 15:14:37 +0300

    final project

--- app/config.py
+++
@@ -1,5 +0,0 @@
-SUBSCRIPTION_KEY = "6473b62078ac4881971689155789c7cc"
-ENDPOINT = "https://cv-captions.cognitiveservices.azure.com/"
-REQUEST_URL = ENDPOINT +"vision/v3.2/describe?maxCandidates=4&language=en&model-version=latest"
-headers = {"Ocp-Apim-Subscription-Key":SUBSCRIPTION_KEY}
-params = {"visualFeatures":"Categories,Description,Color"}
